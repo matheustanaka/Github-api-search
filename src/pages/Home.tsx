0189e3ea024b2cd4de1ef6947f5ef8139af2b580
Matheus Tanaka <matheustanakanog@gmail.com>
2022-01-24 20:50:09 -0300

    feat: add Home page component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import { Header } from "../components/Header";
+import { InputSearch } from "../components/InputSearch";
+import { ButtonSearch } from "../components/ButtonSearch";
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <InputSearch />
+            <ButtonSearch />
+        </>
+
+    )
+}
+
+export default Home;
