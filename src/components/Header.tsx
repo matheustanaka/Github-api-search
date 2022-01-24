9d4a6c6836ea2b6744d821117355370882dd0345
Matheus Tanaka <matheustanakanog@gmail.com>
2022-01-24 20:49:35 -0300

    feat: add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <div className="header-content">
+            <header>
+                <h1>Github Explorer</h1>
+            </header>
+        </div>
+    )
+}
