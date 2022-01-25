e02066badde406ff152124e7b92a5b52036d87be
Matheus Tanaka <matheustanakanog@gmail.com>
2022-01-25 15:49:04 -0300

    refactor: taking off button search component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import { Header } from "../components/Header";
 import { InputSearch } from "../components/InputSearch";
-import { ButtonSearch } from "../components/ButtonSearch";
 
 const Home = () => {
     return (
         <>
             <Header />
             <InputSearch />
-            <ButtonSearch />
         </>
 
     )
